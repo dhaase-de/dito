75300bec40cb38139ee88cf67e838439be0bb4ee
Daniel Haase <git@dhaase.de>
2020-12-16 08:57:56 +0100

    Add functions 'asgray' and 'ascolor'

--- ezcv2/channels.py
+++ ezcv2/channels.py
@@ -1,6 +1,43 @@
+import cv2
+
+
 def isgray(image):
+    """
+    Return `True` iff the given image is a grayscale image.
+    """
+    
     return (len(image.shape) == 2) or ((len(image.shape) == 3) and (image.shape[2] == 1))
 
 
 def iscolor(image):
+    """
+    Return `True` iff the given image is a color image.
+    """
+    
     return (len(image.shape) == 3) and (image.shape[2] == 3)
+
+
+def asgray(image):
+    """
+    Convert the given image from BGR to grayscale.
+    
+    If it is already a grayscale image, return the image unchanged.
+    """
+    
+    if isgray(image=image):
+        return image
+    else:
+        return cv2.cvtColor(src=image, code=cv2.COLOR_BGR2GRAY)
+
+
+def ascolor(image):
+    """
+    Convert the given image from grayscale to BGR.
+    
+    If it is already a color image, return the image unchanged.
+    """
+    
+    if iscolor(image=image):
+        return image
+    else:
+        return cv2.cvtColor(src=image, code=cv2.COLOR_GRAY2BGR)
