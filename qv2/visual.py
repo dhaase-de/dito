24e31c96b730badb6a235360e56901aa3d132540
Daniel Haase <git@dhaase.de>
2021-02-10 12:07:20 +0100

    Add 'is_colormap' function

--- qv2/visual.py
+++ qv2/visual.py
@@ -1,6 +1,24 @@
 import cv2
+import numpy as np
 
+import qv2.channels
 import qv2.data
+
+
+def is_colormap(colormap):
+    """
+    Returns `True` iff `colormap` is a OpenCV-compatible colormap.
+    
+    For this, `colormap` must be a `uint8` array of shape `(256, 1, 3)`, i.e.
+    a color image of size `1x256`.
+    """
+    if not qv2.channels.is_image(image=colormap):
+        return False
+    if colormap.dtype != np.uint8:
+        return False
+    if colormap.shape != (256, 1, 3):
+        return False
+    return True
 
 
 def colorize(image, colormap_name):
