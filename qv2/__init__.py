69a91eb5f990b06c4d7ccc39027b3bf83d942837
Daniel Haase <git@dhaase.de>
2021-02-11 10:16:08 +0100

    Remove empty 'geometry' submodule

--- qv2/__init__.py
+++ qv2/__init__.py
@@ -4,7 +4,6 @@ __version__ = "0.0.1"
 from qv2.aliases import *
 from qv2.core import *
 from qv2.data import *
-from qv2.geometry import *
 from qv2.infos import *
 from qv2.io import *
 from qv2.utils import *
