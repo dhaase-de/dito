4cb66f675720309d3f36d4a23123caa8e0ded8ec
Daniel Haase <git@dhaase.de>
2021-07-06 23:53:22 +0200

    Add filter and find functionality to 'ContourFinder'

--- dito/analysis.py
+++ dito/analysis.py
@@ -1,3 +1,5 @@
+import operator
+
 import cv2
 import numpy as np
 
@@ -22,6 +24,9 @@ class ContourFinder():
         """
         return len(self.contours)
 
+    def __getitem__(self, key):
+        return self.contours[key]
+
     @staticmethod
     def find_contours(image):
         """
@@ -35,7 +40,42 @@ class ContourFinder():
         contours_raw = result[-2]
 
         # return tuple of instances of class `Contour`
-        return tuple(Contour(points=contour_raw[:, 0, :]) for contour_raw in contours_raw)
+        return [Contour(points=contour_raw[:, 0, :]) for contour_raw in contours_raw]
+
+    def filter(self, func, min_value=None, max_value=None):
+        if (min_value is None) and (max_value is None):
+            # nothing to do
+            return
+
+        # filter
+        contours_filtered = []
+        for contour in self.contours:
+            value = func(contour)
+            if (min_value is not None) and (value < min_value):
+                continue
+            if (max_value is not None) and (value > max_value):
+                continue
+            contours_filtered.append(contour)
+        self.contours = contours_filtered
+
+    def filter_area(self, min_value=None, max_value=None):
+        self.filter(func=operator.methodcaller("area"), min_value=min_value, max_value=max_value)
+
+    def filter_perimeter(self, min_value=None, max_value=None):
+        self.filter(func=operator.methodcaller("area"), min_value=min_value, max_value=max_value)
+
+    def find_largest(self):
+        """
+        Returns the index of the largest (area-wise) contour.
+        """
+        max_area = None
+        argmax_area = None
+        for (n_contour, contour) in enumerate(self.contours):
+            area = contour.area()
+            if (max_area is None) or (area > max_area):
+                max_area = area
+                argmax_area = n_contour
+        return argmax_area
 
 
 def contours(image):
