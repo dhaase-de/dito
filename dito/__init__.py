e751a92a71d3987332578571ab440029fe3b560a
Daniel Haase <git@dhaase.de>
2021-07-10 22:30:56 +0200

    Rename 'analysis.py' -> 'processing.py'

--- dito/__init__.py
+++ dito/__init__.py
@@ -2,10 +2,10 @@ __version__ = "2.4.0.dev0"
 
 
 from dito.aliases import *
-from dito.analysis import *
 from dito.core import *
 from dito.data import *
 from dito.infos import *
 from dito.io import *
+from dito.processing import *
 from dito.utils import *
 from dito.visual import *
