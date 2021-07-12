cc6336110c7d1c7e0bc7be614e8c4205f3368bea
Daniel Haase <git@dhaase.de>
2021-07-12 23:48:12 +0200

    Rename 'infos.py' -> 'inspect.py'

--- dito/__init__.py
+++ dito/__init__.py
@@ -3,7 +3,7 @@ __version__ = "2.4.0.dev0"
 
 from dito.core import *
 from dito.data import *
-from dito.infos import *
+from dito.inspect import *
 from dito.io import *
 from dito.processing import *
 from dito.utils import *
