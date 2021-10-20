48694a080ccecdd374204fd75e121fbdad008f20
Daniel Haase <git@dhaase.de>
2021-10-20 02:01:31 +0200

    Add 'parallel' submodule with function 'mp_starmap'

--- dito/__init__.py
+++ dito/__init__.py
@@ -8,6 +8,7 @@ from dito.exceptions import *
 from dito.highgui import *
 from dito.inspect import *
 from dito.io import *
+from dito.parallel import *
 from dito.processing import *
 from dito.utils import *
 from dito.visual import *
