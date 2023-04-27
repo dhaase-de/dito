6f7324405f902743bb22f6db9831ac23babce1b4
Daniel Haase <git@dhaase.de>
2023-04-27 22:00:31 +0200

    Add overview of submodules in main docstring

--- dito/__init__.py
+++ dito/__init__.py
@@ -13,9 +13,29 @@ The module follows the data conventions of OpenCV under Python, namely:
 * point coordinates are given in (x, y[, z]) order
 * images sizes (not shapes--these have the same meaning as in NumPy) are given
   in (width, height) order
+* arguments such as `line_type`, `interpolation`, etc. expect values defined by
+  OpenCV (e.g., `cv2.LINE_AA`, `cv2.INTER_LINEAR`, etc.)
 
 All submodules are imported and can be accessed directly through the `dito`
 namespace. For example, `dito.io.load` can be accessed as `dito.load`.
+
+The code is structured into the following submodules:
+
+Submodule         | Description                                                       | Example
+------------------|-------------------------------------------------------------------|--------------------------------
+`dito.analysis`   | higher-level image analysis tasks                                 | `dito.analysis.PcaTextureModel`
+`dito.conversion` | conversion of images represented as NumPy images to other formats | `dito.conversion.to_PySide6_QPixmap`
+`dito.core`       | core image-related functionality                                  | `dito.core.normalize`
+`dito.data`       | pre-defined images and other data (colormaps, fonts, etc.)        | `dito.data.pm5544`
+`dito.draw`       | basic image drawing                                               | `dito.draw.draw_symbol`
+`dito.exceptions` | exception classes used within `dito`                              | `dito.exceptions.ConversionError`
+`dito.highgui`    | extensions for OpenCV's built-in `highgui` module                 | `dito.highgui.FloatSlider`
+`dito.inspection` | functions for inspecting images and their properties              | `dito.inspection.pinfo`
+`dito.io`         | low-level image input/output functions                            | `dito.io.load`
+`dito.parallel`   | functions for parallelizing image processing                      | `dito.parallel.mp_starmap`
+`dito.processing` | basic image processing                                            | `dito.processing.contours`
+`dito.utils`      | utility functions used throughout `dito`                          | `dito.utils.now_str`
+`dito.visual`     | functions for visualizing images                                  | `dito.visual.text`
 """
 
 __version__ = "2.9.1.dev0"
