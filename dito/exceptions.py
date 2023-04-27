7a90da912796f993b0155f432f5ab1a688d0937e
Daniel Haase <git@dhaase.de>
2023-04-27 23:11:47 +0200

    Add docstrings for dito.exceptions

--- dito/exceptions.py
+++ dito/exceptions.py
@@ -1,5 +1,14 @@
-class DitoException(Exception): pass
+class DitoException(Exception):
+    """Base exception class for Dito."""
 
-class ConversionError(DitoException): pass
-class InvalidImageShapeError(DitoException): pass
-class QkeyInterrupt(DitoException): pass
+
+class ConversionError(DitoException):
+    """Raised when a conversion to a different data type fails."""
+
+
+class InvalidImageShapeError(DitoException):
+    """Raised when an image has an invalid shape, e.g. too few or too many dimensions."""
+
+
+class QkeyInterrupt(DitoException):
+    """Raised when the user presses the Q key during an image display, causing the display to be closed."""
