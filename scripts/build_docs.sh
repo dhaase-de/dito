5b5331e70cb7cff29936e5eb151c137fa993a2ea
Daniel Haase <git@dhaase.de>
2023-05-12 20:56:42 +0200

    Add version number to documentation

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -7,6 +7,8 @@ set -o nounset
 # get absolute path of this script
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
+DITO_VERSION="$(echo "import dito; print(dito.__version__)" | /usr/bin/env python3)"
+
 source "$SCRIPT_DIR"/setenv.sh
-cd "$PACKAGE_DIR" && pdoc dito --output-directory=docs --docformat=numpy --footer-text="dito"
+cd "$PACKAGE_DIR" && pdoc dito --output-directory=docs --docformat=numpy --footer-text="dito v$DITO_VERSION"
 
