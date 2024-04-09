4bed140441ae95eb92cca76f877438af9016d6db
Daniel Haase <git@dhaase.de>
2024-04-09 14:17:48 +0200

    Bump version to 2.12.0

--- dito/__init__.py
+++ dito/__init__.py
@@ -42,7 +42,7 @@ Submodule         | Description                                                 
 `dito.visual`     | functions for visualizing images                                  | `dito.visual.text`
 """
 
-__version__ = "2.11.2.dev0"
+__version__ = "2.12.0"
 
 
 from dito.analysis import *
